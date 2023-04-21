7c5293d190b22290bd530c2f8a40c61828ce18c6
Kieran Sagar Parikh <kieran@parikh.ca>
2023-04-20 23:50:49 -0400

    working physics

--- src/index.js
+++ src/index.js
@@ -15,22 +15,111 @@
 // const P5 = new p5(sketch);
 import Matter from 'matter-js'
 
-let orig = null;
-setup();
-loop();
+class App {
+    orig = null;
+    engine = null;
+    windowElement = null;
+    animationsInProgress = [];
+    isAnimating = false;
 
-function setup() {
-    dragElement(document.getElementById("window"));
-    closeButton();
-    minimizeButton();
-    resizeButton();
+    constructor() {
+        dragElement(document.getElementById("window"));
+        closeButton();
+        minimizeButton();
+        this.resizeButton();
+        this.specialButton();
+
+        this.windowElement = document.getElementById("window")
+
+        this.engine = Matter.Engine.create()
+
+        this.windowDims = this.windowElement.getBoundingClientRect()
+        this.windowPhysicsBox = Matter.Bodies.rectangle(this.windowDims.x + (this.windowDims.width/2), this.windowDims.y + (this.windowDims.height/2), this.windowDims.width , this.windowDims.height, {
+            restitution: 0.7,
+            density: 0.02,
+        })
+        this.floorPlane = Matter.Bodies.rectangle(0, window.innerHeight, window.innerWidth, 1, {
+            isStatic: true,
+            friction: 0.2,
+        })
+       // Matter.Body.rotate(this.windowPhysicsBox, Math.PI/2)
+
+
+        Matter.Composite.add(this.engine.world, [
+            this.windowPhysicsBox,
+            this.floorPlane,
+        ])
+
+        // var render = Matter.Render.create({
+        //     element: document.body,
+        //     engine: this.engine,
+        //     options: {
+        //         width: window.innerWidth,
+        //         height: window.innerHeight,
+        //         showVelocity: true
+        //     }
+        // });
+        // Matter.Render.run(render);
+        // this.windowElement.style.top = (this.windowPhysicsBox.position.y - (windowDims.height/2)) + "px"
+        // this.windowElement.style.left = (this.windowPhysicsBox.position.x - (windowDims.width/2)) + "px"
+        // let degrees = this.windowPhysicsBox.angle * (180 / Math.PI)
+        // this.windowElement.style.transform = `rotate(${degrees}deg)`
+    }
+
+    loop() {
+        currentTime();
+        if (this.isAnimating) {
+            Matter.Engine.update(this.engine, 1000 / 60);
+
+            // Matter.Composite.add(game.getWorld(), this.physicsBox)
+
+            // for (const animation of this.animationsInProgress) {
+            //     animation.animate()
+            // }
+
+            // let
+            // document.getElementById("window").style.width = "auto"
+            // document.getElementById("window").style.width = "auto"
+            this.windowElement.style.top = (this.windowPhysicsBox.position.y - (this.windowDims.height/2)) + "px"
+            this.windowElement.style.left = (this.windowPhysicsBox.position.x - (this.windowDims.width/2)) + "px"
+            this.windowElement.style.transform = `rotate(${this.windowPhysicsBox.angle}rad)`
+        }
+
+        requestAnimationFrame(() => this.loop())
+    }
+
+    specialButton() {
+        document.getElementById("specialButton").onclick = () => {
+            this.isAnimating = true;
+        };
+    }
+
+    resizeButton() {
+        document.getElementById("resizeButton").onclick = () => {
+            if (this.orig === null) {
+                this.orig = [
+                    document.getElementById("window").style.top,
+                    document.getElementById("window").style.left,
+                    document.getElementById("windowContent").clientWidth,
+                    document.getElementById("windowContent").clientHeight,
+                ]
+                document.getElementById("window").style.top = "30px"
+                document.getElementById("window").style.left = "15px"
+                document.getElementById("windowContent").style.width = (window.innerWidth - 40) + "px";
+                document.getElementById("windowContent").style.height = (window.innerHeight - 70) + "px";
+            } else {
+                document.getElementById("window").style.top = this.orig[0]
+                document.getElementById("window").style.left = this.orig[1]
+                document.getElementById("windowContent").style.width = this.orig[2] + "px"
+                document.getElementById("windowContent").style.height = this.orig[3] + "px"
+                this.orig = null;
+            }
+        };
+    }
+
+
 }
 
-function loop() {
-    currentTime();
-
-    requestAnimationFrame(() => loop())
-}
 function closeButton() {
     document.getElementById("closeButton").onclick = function () {
         document.getElementById("window").style.display = "none";
@@ -49,30 +138,6 @@ function minimizeButton() {
     };
 }
 
-function resizeButton() {
-    document.getElementById("resizeButton").onclick = function () {
-        if (orig === null) {
-            orig = [
-                document.getElementById("window").style.top,
-                document.getElementById("window").style.left,
-                document.getElementById("windowContent").clientWidth,
-                document.getElementById("windowContent").clientHeight,
-            ]
-            document.getElementById("window").style.top = "30px"
-            document.getElementById("window").style.left = "15px"
-            document.getElementById("windowContent").style.width = (window.innerWidth - 40) + "px";
-            document.getElementById("windowContent").style.height = (window.innerHeight - 70) + "px";
-        } else {
-            console.log(orig)
-            document.getElementById("window").style.top = orig[0]
-            document.getElementById("window").style.left = orig[1]
-            document.getElementById("windowContent").style.width = orig[2] + "px"
-            document.getElementById("windowContent").style.height = orig[3] + "px"
-            orig = null;
-        }
-
-    };
-}
 
 function dragElement(elmnt) {
     var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
@@ -141,3 +206,6 @@ function currentTime() {
 
     document.getElementById("time").innerText = time;
 }
+
+const app = new App()
+app.loop()
